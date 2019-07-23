3a7a7ecbbb998488b5b0f5b84588b103ae503a28
Halocaster <halocaster2005@gmail.com>
2019-07-23 20:48:21 +0800

    Added the projectile

---
+++ Projectiles/Aeroslice.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Halorealm.Projectiles
+{
+    public class Aeroslice : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Aeroslice");
+            ProjectileID.Sets.YoyosLifeTimeMultiplier[projectile.type] = -1f;
+            ProjectileID.Sets.YoyosMaximumRange[projectile.type] = 275f;
+            ProjectileID.Sets.YoyosTopSpeed[projectile.type] = 11f;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.melee = true;
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.aiStyle = 99;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.scale = 1f;
+            projectile.extraUpdates = 0;
+        }
+
+        public override void PostAI()
+        {
+            if (Main.rand.NextBool())
+            {
+                Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 16);
+                dust.noGravity = true;
+                dust.scale = 1.6f;
+            }
+        }
+    }
+}
