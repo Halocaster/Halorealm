645b863d8bddf2a8ec860f8e00b0a6642716e640
Halocaster <halocaster2005@gmail.com>
2019-07-28 14:04:58 +0800

    Updated the code

--- HalorealmPlayer.cs
+++ HalorealmPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -9,12 +10,32 @@ namespace Halorealm
         public bool vulcrainCharm;
         public bool hellstoneCharm;
         public bool frostfireEmblem;
+        public bool darkshadeBurn;
 
         public override void ResetEffects()
         {
             vulcrainCharm = false;
             hellstoneCharm = false;
             frostfireEmblem = false;
+            darkshadeBurn = false;
+        }
+
+        public override void UpdateDead()
+        {
+            darkshadeBurn = false;
+        }
+
+        public override void UpdateBadLifeRegen()
+        {
+            if (darkshadeBurn)
+            {
+                if (player.lifeRegen > 0)
+                {
+                    player.lifeRegen = 0;
+                }
+                player.lifeRegenTime = 0;
+                player.lifeRegen -= 20;
+            }
         }
 
         public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
@@ -71,6 +92,25 @@ namespace Halorealm
             {
                 target.AddBuff(BuffID.Frostburn, 120);
                 target.AddBuff(BuffID.OnFire, 120);
+            }
+        }
+
+        public override void DrawEffects(PlayerDrawInfo drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
+        {
+            if (darkshadeBurn)
+            {
+                if (Main.rand.NextBool(4) && drawInfo.shadow == 0f)
+                {
+                    int dust = Dust.NewDust(drawInfo.position - new Vector2(2f, 2f), player.width + 4, player.height + 4, mod.DustType("DarkshadeBurn"), player.velocity.X * 0.4f, player.velocity.Y * 0.4f, 100, default(Color), 3f);
+                    Main.dust[dust].noGravity = true;
+                    Main.dust[dust].velocity *= 1.8f;
+                    Main.dust[dust].velocity.Y -= 0.5f;
+                    Main.playerDrawDust.Add(dust);
+                }
+                r *= 0.2f;
+                g *= 0.1f;
+                b *= 0.7f;
+                fullBright = true;
             }
         }
     }
--- NPCs/HalorealmGlobalNPC.cs
+++ NPCs/HalorealmGlobalNPC.cs
@@ -1,10 +1,34 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace Halorealm.NPCs
 {
     public class HalorealmGlobalNPC : GlobalNPC
     {
+        public bool darkshadeBurn;
+
+        public override void ResetEffects(NPC npc)
+        {
+            darkshadeBurn = false;
+        }
+
+        public override void UpdateLifeRegen(NPC npc, ref int damage)
+        {
+            if (darkshadeBurn)
+            {
+                if (npc.lifeRegen > 0)
+                {
+                    npc.lifeRegen = 0;
+                }
+                npc.lifeRegen -= 20;
+                if (damage < 2)
+                {
+                    damage = 2;
+                }
+            }
+        }
+
         public override void NPCLoot(NPC npc)
         {
             if (npc.lifeMax > 5 && npc.value > 0f) //edit to make it drop from enemies after killing EoC
@@ -13,6 +37,26 @@ namespace Halorealm.NPCs
                 {
                     Item.NewItem(npc.getRect(), mod.ItemType("HalocraticEssence"));
                 }
+            }
+        }
+
+        public override void DrawEffects(NPC npc, ref Color drawColor)
+        {
+            if (darkshadeBurn)
+            {
+                if (Main.rand.Next(4) < 3)
+                {
+                    int dust = Dust.NewDust(npc.position - new Vector2(2f, 2f), npc.width + 4, npc.height + 4, mod.DustType("DarkshadeBurn"), npc.velocity.X * 0.4f, npc.velocity.Y * 0.4f, 100, default(Color), 3.5f);
+                    Main.dust[dust].noGravity = true;
+                    Main.dust[dust].velocity *= 1.8f;
+                    Main.dust[dust].velocity.Y -= 0.5f;
+                    if (Main.rand.NextBool(4))
+                    {
+                        Main.dust[dust].noGravity = false;
+                        Main.dust[dust].scale *= 0.5f;
+                    }
+                }
+                Lighting.AddLight(npc.position, 0.2f, 0.1f, 0.7f);
             }
         }
     }
