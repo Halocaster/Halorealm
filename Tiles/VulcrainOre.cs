16d6893ad96ddc82d64a774fde49b0dfad9f4614
Halocaster <halocaster2005@gmail.com>
2019-07-30 16:11:10 +0800

    Added to the mod

---
+++ Tiles/VulcrainOre.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Halorealm.Tiles
+{
+    public class VulcrainOre : ModTile
+    {
+        public override void SetDefaults()
+        {
+            TileID.Sets.Ore[Type] = true;
+            Main.tileSpelunker[Type] = true; 
+            Main.tileValue[Type] = 550;
+            Main.tileShine2[Type] = true;
+            Main.tileShine[Type] = 800;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Vulcrain Ore");
+            AddMapEntry(new Color(22, 75, 201), name);
+            dustType = 84;
+            drop = mod.ItemType("VulcrainOre");
+            soundType = 21;
+            soundStyle = 1;
+            minPick = 55;
+        }
+    }
+}
