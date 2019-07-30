dc0451193a85b5b673048bc0b9372c9159d6d048
Halocaster <halocaster2005@gmail.com>
2019-07-30 22:05:55 +0800

    Added the ModifyTooltips hook

--- Tweaks/GoldPickaxe.cs
+++ Tweaks/GoldPickaxe.cs
@@ -1,11 +1,28 @@
-﻿using Terraria;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Halorealm.Tweaks
 {
-    public class GoldPickaxe : GlobalItem //try to change the item's vanilla tooltip
+    public class GoldPickaxe : GlobalItem 
     {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.type == ItemID.GoldPickaxe)
+            {
+                tooltips.RemoveAll(x => x.Name == "Tooltip0" && x.mod == "Terraria");
+                TooltipLine line = tooltips.FirstOrDefault(x => x.Name == "Tooltip0" && x.mod == "Terraria");
+                if (line != null)
+                {
+                    line.text = "Can mine Meteorite and Vulcrain";
+                }
+
+            }
+        }
+        //special thanks to IceCoffe for the help
+
         public override void SetDefaults(Item item)
         {
             if (item.type == ItemID.GoldPickaxe)
