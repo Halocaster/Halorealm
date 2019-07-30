6c430a494fb23835ec0f91a1c219cf8678205d1d
Halocaster <halocaster2005@gmail.com>
2019-07-30 16:44:40 +0800

    Added to the mod

---
+++ Projectiles/TheShadow.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Halorealm.Projectiles
+{
+    public class TheShadow : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Shadow");
+            Main.projFrames[projectile.type] = 5;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 40;
+            projectile.height = 40;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = false;
+            projectile.alpha = 175;
+            projectile.penetrate = 7;
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.Black;
+        }
+    }
+}
