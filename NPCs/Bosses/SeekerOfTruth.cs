ecc4c0143becbfd5ea1a603134b6a22af4273702
Halocaster <halocaster2005@gmail.com>
2019-07-31 06:42:06 +0800

    Created skeleton of boss NPC

---
+++ NPCs/Bosses/SeekerOfTruth.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Halorealm.NPCs.Bosses
+{
+    public class SeekerOfTruth : ModNPC
+    {
+        public override void SetDefaults()
+        {
+
+        }
+
+        public override void AI() 
+        {
+
+        }
+    }
+}
