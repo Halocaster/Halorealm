b3a3657a2e58ebb8fd2d25e5fb97110d127ef427
Halocaster <halocaster2005@gmail.com>
2019-07-30 16:26:20 +0800

    Changed the vanilla Gold Pickaxe

---
+++ Tweaks/GoldPickaxe.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Halorealm.Tweaks
+{
+    public class GoldPickaxe : GlobalItem //try to change the item's vanilla tooltip
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == ItemID.GoldPickaxe)
+            {
+                item.pick = 59;
+            }
+        }
+    }
+}
