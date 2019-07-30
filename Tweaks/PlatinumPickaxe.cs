e2209f8e769d43c82f617ad970f73486988c2c02
Halocaster <halocaster2005@gmail.com>
2019-07-30 22:08:16 +0800

    Added to the mod

---
+++ Tweaks/PlatinumPickaxe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Halorealm.Tweaks
+{
+    public class PlatinumPickaxe : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.type == ItemID.PlatinumPickaxe)
+            {
+                tooltips.RemoveAll(x => x.Name == "Tooltip0" && x.mod == "Terraria");
+                TooltipLine line = tooltips.FirstOrDefault(x => x.Name == "Tooltip0" && x.mod == "Terraria");
+                if (line != null)
+                {
+                    line.text = "Can mine Meteorite and Vulcrain";
+                }
+
+            }
+        }
+        //special thanks to IceCoffe for the help
+    }
+}
