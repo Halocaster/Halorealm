7aed733c566514e0a6a6a3ef0bf5299abff0c108
Halocaster <halocaster2005@gmail.com>
2019-08-22 18:50:27 +0800

    Edited the code (started to make the mod again)

--- Items/Darkshade/Armor/DarkshadeMask.cs
+++ Items/Darkshade/Armor/DarkshadeMask.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace Halorealm.Items.Darkshade.Armor
 {
-    class DarkshadeMask
+    [AutoloadEquip(EquipType.Head)]
+    public class DarkshadeMask : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Darkshade Mask");
+            Tooltip.SetDefault("10% increased ranged damage" +
+                "\n 5% increased ranged crit chance");
+        }
+
+        public override void SetDefaults()
+        {
+            item.defense = 9;
+            item.width = 30;
+            item.height = 24;
+            item.value = Item.sellPrice(0, 1, 5, 0);
+            item.rare = 3;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == mod.ItemType("DarkshadeBreastplate") && legs.type == mod.ItemType("DarkshadeLeggings");
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.rangedDamage += 0.1f;
+            player.rangedCrit += 5;
+        }
+
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = "Minor increase in physical and mental stats";
+            player.statDefense += 5;
+            player.lifeRegen += 15;
+            player.rangedCrit += 5;
+            player.rangedDamage += 0.05f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("DarkshadeBar"), 25);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
     }
 }
